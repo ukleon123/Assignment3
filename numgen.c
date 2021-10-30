0f5372272cb10c00b65dd96cee9100e953ce3dc8
7masterpiece! <ukleon123@gmail.com>
2021-10-30 12:14:41 +0900

    add numgen.c to generate num

---
+++ numgen.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+#define MAX_PROCESS 4
+int main(){
+	FILE* f_write = fopen("temp.txt", "w");
+	for(int i = 0; i < MAX_PROCESS * 2; i ++){
+		fprintf(f_write, "%d\n", i + 1);
+	}
+	fclose(f_write);
+}
