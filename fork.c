534cdd1b195c991935c7b96e6d8591101c756206
Sung Woo Choi <ukleon123@gmail.com>
2021-11-04 21:36:24 +0900

    complete thread

--- fork.c
+++ fork.c
@@ -2,39 +2,55 @@
 #include<stdio.h>
 #include<stdint.h>
 #include<pthread.h>
+#include<signal.h>
+
+#pragma warning(disable:4996)
 
 #define MAX_PROCESS 4
-typedef struct {
-	int data[2];
-	FILE* fd;
-}calc;
 
-void *add(void* data){
-	calc* act_data = data;
-	int ret = (act_data->data[0] + act_data->data[1]);
-	printf("%d\n", ret);
-	fprintf(act_data->fd, "%d\n", ret);
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+int offset;
+void* add(void* data) {
+	int ret;
+	char line[10000];
+	
+	pthread_mutex_lock(&lock);
+	FILE* text = fopen("temp.txt", "r");
+	fseek(text, offset, SEEK_SET);
+	
+	ret = 0;
+	
+	for (int k = 0; k < 2; k++) {
+		fgets(line, 10000, text);
+		ret += atoi(line);
+	}
+	offset = ftell(text);
+	fclose(text);
+	pthread_mutex_unlock(&lock);
+	
+	return ret;
 }
 
-void main(){
+void main() {
 
 	pthread_t pthread[MAX_PROCESS];
-	void* ret;
-	char* one;
-	char line[3];
+
+	void* data;
+	int error;
 	
-	calc* data = malloc(sizeof(calc));
-	for (float i = MAX_PROCESS; i >= 1; i /= 2){
-		FILE* text = fopen("temp.txt", "a+");
-		data->fd = text;
-		for (int j = 0; j < i; j++){
-			for(int k = 0; k < 2; k++){
-				one = fgets(line, 3, text);
-				data->data[k] = one[0];
+	for (float i = MAX_PROCESS; i >= 1; i /= 2) {
+		for (int j = 0; j < i; j++) {
+			FILE* text = fopen("temp.txt", "a");
+			error = pthread_create(&pthread[j], NULL, add, NULL);
+			
+			if (error < 0) printf("error");
+			else {
+				pthread_join(pthread[j], &data);
+				fprintf(text, "%d\n", (int)data);
+				fclose(text);
 			}
-			pthread_create(&pthread[j], NULL, add, (void *)data);
-			pthread_detach(pthread[j]);
 		}
-		fclose(text);
-	}	
+	}
 }
